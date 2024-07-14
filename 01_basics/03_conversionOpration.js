bd8c288c0b77642765b890bf8de18b086f0a9d53
VikasManhinde <115807854+VikasManhinde@users.noreply.github.com>
2024-07-14 19:55:30 +0000

    this is complete now

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,34 @@
+let score = "vikas"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" = 33
+// "33abc" => Nan (Not in number)
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// " " => true
+// "vikas" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// Please start the video from 1:14:50 this time to learn next chapeter
