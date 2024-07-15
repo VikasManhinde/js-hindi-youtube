39c21ac62ac03ac06ddb295a304c79785797b5cd
VikasManhinde <115807854+VikasManhinde@users.noreply.github.com>
2024-07-15 18:56:40 +0000

    start

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -22,13 +22,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // " " => true
 // "vikas" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// 
 
-// Please start the video from 1:14:50 this time to learn next chapeter
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
