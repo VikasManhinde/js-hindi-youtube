a60b1218b05ccc56febbcbf06567736eb26c254a
VikasManhinde <115807854+VikasManhinde@users.noreply.github.com>
2024-07-15 19:09:13 +0000

    done coversion and opration

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -49,11 +49,27 @@ let str2 = " Hitesh"
 
 let str3 = str1 + str2
 
-console.log(str3);
+// console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
 
+// console.log((3 + 4 ) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+++gameCounter; // Prefix
+gameCounte++; // Postfix
+console.log(gameCounter);
+
+// Prefix & Postfix
+// HomeWork of Increment, search js and MDN read it lern some example and study ( Prefix & Postfix )
