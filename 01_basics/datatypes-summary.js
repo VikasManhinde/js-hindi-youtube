d130828646e8f700a75c565bf27e3da3d17ed76c
VikasManhinde <115807854+VikasManhinde@users.noreply.github.com>
2024-07-15 20:13:19 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Premitive Data types
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // This will note as Undefined value
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3487458329485794n // bigInt
+
+
+
+// Reference ( Non Primitive ) Data types
+
+// Array, Objescts, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "ViKas",
+    age: 21,
+}
+
+const myFuction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFuction);
+console.log(typeof heros);
+
+// Assignment
+
+// You have to check typeof All the DataTypes given above 
+// This various typeof questions aks in interview so be ready 
