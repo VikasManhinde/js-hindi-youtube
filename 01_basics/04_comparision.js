da324c67f1dd3a410ccb6c1f622ad7600b153fb8
VikasManhinde <115807854+VikasManhinde@users.noreply.github.com>
2024-07-15 19:36:19 +0000

    Done for the topic

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,9 +1,23 @@
+// Number and Number - This is the Finest comparion
 // console.log(2 > 1);
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// String and Number  - This comparision can be confusing some of the time
+// console.log("2" > 1);
+// console.log("02" > 1);
 
-console.log("2" > 1);
-console.log("02" > 1);
+// This is very confusing avoid this type of code, this make (conversion while comparision)
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
